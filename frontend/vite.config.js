1f3cb7a0a61312686b5f5378fc63ef3f6b869086
BomXanhG-Z <haodo285@gmail.com>
2026-03-31 12:39:58 +0900

    last complete

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 
 // --- GLOBAL CONFIGURATION VARIABLES ---
 const API_PREFIX = '/api'
-const { VITE_PORT, VITE_API_TARGET } = import.meta.env
 
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    port: VITE_PORT,
-    proxy: {
-      [API_PREFIX]: {
-        target: VITE_API_TARGET,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [vue()],
+    server: {
+      port: env.VITE_PORT,
+      proxy: {
+        [API_PREFIX]: {
+          target: env.VITE_API_TARGET,
+        },
       },
     },
-  },
+  }
 })
-
